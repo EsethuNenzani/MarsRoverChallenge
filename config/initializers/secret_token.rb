c42a3c7f470d34282e05eb047fe4de74b6fc61f1
EsethuNenzani <nenzani.ea@gmail.com>
2021-03-29 13:07:25 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarsRoverChallenge::Application.config.secret_token = '0586b777c70ce6a03619ab79f390b7f3ff6e075ad3d615569a3863befc959e52a5dfd6318b42e507d04798439105e351f5d017b84ecb062d70dbd5af70a026e8'
