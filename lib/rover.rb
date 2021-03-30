3f75ed6c7bf4713361a2bb3bef5e695e5a862d18
EsethuNenzani <nenzani.ea@gmail.com>
2021-03-30 09:53:08 +0200

    Adding move_direction functionality

--- lib/rover.rb
+++ lib/rover.rb
@@ -3,17 +3,26 @@ class Rover
 
   def move_rover(instructions_string)
     instructions = instructions_string.split("")
-    left_position = []
-    right_position = []
-    up_position = ""
+    new_x_coordinate = @x_coordinate
+    new_y_coordinate = @y_coordinate
+    new_compass_position = []
     position = @compass_point
     #keep track of position or update position
     instructions.each do |instruction|
       if instruction == "L"
-        left_position << turn_left(position)
+        new_compass_position << turn_left(position)
         position = turn_left(position)
+      elsif instruction == "R"
+        new_compass_position << turn_right(position)
+        position = turn_right(position)
+      else
+        new_x_coordinate = move_direction_x(new_x_coordinate, position)
+        new_y_coordinate = move_direction_y(new_y_coordinate, position)
       end
     end
+
+    [new_x_coordinate, new_y_coordinate, new_compass_position.last]
+
   end
 
   def turn_left(position)
@@ -30,24 +39,36 @@ class Rover
   end
 
   def turn_right(position)
-    cardinal_position = ""
-      if position == 'E'
-        cardinal_position ==  'S'
-      elsif position == 'S'
-        cardinal_position == 'W'
-      elsif position == 'W'
-        cardinal_position ==  'N'
-      else
-        cardinal_position ==  'E'
-      end
+    if position == 'E'
+      cardinal_position =  'S'
+    elsif position == 'S'
+      cardinal_position = 'W'
+    elsif position == 'W'
+      cardinal_position =  'N'
+    else
+      cardinal_position =  'E'
+    end
     cardinal_position
   end
 
-  def move_up(position)
-  #   if left  -1 in x_axis (W)
-  #   if right + 1 in x_axis (E)
-  #   if down - 1 (S)
-  #   if up + 1 (N)
+  def move_direction_x(x_coordinate, position)
+    new_x_coordinate = x_coordinate
+    if position == "E"
+      new_x_coordinate = new_x_coordinate + 1
+    elsif position == "W"
+      new_x_coordinate = new_x_coordinate - 1
+    end
+    new_x_coordinate
+  end
+
+  def move_direction_y(y_coordinate, position)
+    new_y_coordinate = y_coordinate
+    if position == "N"
+      new_y_coordinate = new_y_coordinate + 1
+    elsif position == "S"
+      new_y_coordinate = new_y_coordinate - 1
+    end
+    new_y_coordinate
   end
 
 
